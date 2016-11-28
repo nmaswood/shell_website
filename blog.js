e864487464d63c7cd71d3413e95691600c00e255
Nasr Maswood <nmaswood@uchicago.edu>
2016-11-27 21:10:56 -0600

    fjklajsdf

---
+++ blog.js
@@ -0,0 +1,40 @@
+"use strict";
+
+class Article{
+
+    constructor(url, title, date, category){
+        this.url = url;
+        this.title = title;
+        this.date = date;
+        this.category = category;
+    }
+
+    toHTMLElement(){
+
+        const doc = document;
+        const article = doc.createElementById("div");
+        article.setAttribute("class", "article");
+
+        article.appendChild(a);
+
+        const a = doc.createElementById("a");
+        a.setAttribute("href", `${this.url}.html`);
+        a.innerHTML = this.title;
+
+        return article;
+    }
+}
+
+// constructor(url, title, date, category){
+const articles = [
+    new Article('foo', 'bar', 'bar', 'bar')
+]
+
+function init(){
+
+
+}
+
+
+
+
--- main.js
+++ main.js
@@ -1,7 +1,112 @@
+"use strict";
+
+function makeid(){
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const len = possible.length;
+
+        function randomCharacter(){
+            return possible.charAt(Math.floor(Math.random() * len));
+        }
+        
+        return Array.apply(null, {length: 10}).map(randomCharacter).join("");
+}
+
+class Output{
+
+    constructor(result, error){
+        this.result = result;
+        this.error = error;
+    }
+
+    isError(){
+        return this.error != "";
+
+    }
+}
+
+class File {
+    constructor(name, type, children, content){
+        this.name = name;
+        this.type = type;
+        this.children = children;
+        this.content = content;
+        this.uniqueID = `${name}${makeid()}`;
+    }
+
+    fileInfo(){
+        console.log([this.name,this.type, this.uniqueID]);
+    }
+
+    getNames (){
+        return this.children.map((x) => x.name);
+    }
+
+    fileExists(name){
+        return this.getNames().indexOf(name) != -1;
+    }
+
+    getFile(name){
+        console.log(name);
+        console.log(this.children);
+        console.log(this.children[this.children.indexOf(name)]);
+
+        return this.children[this.getNames().indexOf(name)];
+    }
+
+    followPath(path){
+
+        let current = this;
+        let error = false;
+
+        while (path.length){
+            let next = path.shift();
+            let name = next.name;
+            if (!next.fileExists(name)){
+                error = "File does not exist";
+                break;
+            } else {
+                let file = current.getFile(name);
+                if (file.type == 'folder'){
+                    error = "Folder does not exist";
+                    break;
+                }
+                current = file;
+            }
+        }
+
+        return new Output(current, error);
+    }
+}
+
 let ID = 0;
 let currentID = 0;
 
+let currentDir = new File('Home', 'Dir', [
+
+    new File('about.txt', 'file', null, 
+            "Hi! My name is Nasr. I'm a CS major at UChicago.\n" + 
+            "I'm interested in education, programming and a couple of other things too.\n" +
+            'If you are interested in reaching out type &nbsp;<mark class="code">cat contact.txt</mark> &nbsp; to see my info'
+    ),
+    new File('contact.txt', 'file', null, 'personal: nasrmaswood@gmail.com\nschool: nmaswood@uchicago.edu\n github:<a href = "https://github.com/nmaswood">https://github.com/nmaswood</a>'),
+    new File('skills.txt', 'file', null, "Python\nJavascript\nAngularJs\nRails\nGolang\nFree style Rapping ;)"),
+    new File('misc.txt', 'file', null, 'Programming Interview Questions: <a href = "#">https://www.youtube.com/user/TheNasrmaswood</a>')
+]);
+
 const files = ['about.txt', 'contact.txt', "skills.txt", "misc.txt"];
+
+let rootDir = currentDir;
+
+const commands= ['ls', 'cat', 'pwd', 'clear', 'echo', 'whoami', 'date'];
+const manuals = {
+    'cat':'cat arg_0, arg_1 ... will print the contents of arg_0 arg_1',
+    'ls':'list all the files in a directory',
+    'pwd':'Print the current working directory',
+    'clear':'clear the page of all previous output',
+    'echo':'echo arg_0, arg_1 will echo back the args you put in',
+    'whoami':'Simple answer to a deep philosophical question',
+    'cmds': 'What commands are implemented'
+}
 
 const blurbs  = {
     'about.txt': "Hi! My name is Nasr. I'm a CS major at UChicago\n" +
@@ -9,8 +114,9 @@ const blurbs  = {
                  "If you are interested in reaching out " + 
                  'type &nbsp;<mark class="code">cat contact.txt</mark> &nbsp; to see my info',
 
-    'contact.txt': "personal: nasrmaswood@gmail.com\nschool: nmaswood@uchicago.edu\ngithub:",
-    'skills.txt': "Python\nJavascript\nAngularJs\nRails\nGolang\n"
+    'contact.txt': 'personal: nasrmaswood@gmail.com\nschool: nmaswood@uchicago.edu\n github:<a href = "https://github.com/nmaswood">https://github.com/nmaswood</a>',
+    'skills.txt': "Python\nJavascript\nAngularJs\nRails\nGolang\nFree style Rapping ;)",
+    'misc.txt': 'Programming Interview Questions: <a href = "#">https://www.youtube.com/user/TheNasrmaswood</a>'
 }
 
 const handlers = {
@@ -24,24 +130,130 @@ const handlers = {
     cat: (input) => {
 
         for (let i = 0; i < input.length;i++){
-            let word = input[i];
 
-            let output = word in blurbs? blurbs[input[i]]: `cat ${word}: No such file or directory`;
+            let output = (function(){
+
+                const token = input[i];
+
+                if (currentDir.getNames().indexOf(token) != -1){
+                    return currentDir.getFile(token).content;
+                }
+                console.log(token);
+                console.log(token in currentDir.getNames());
+                console.log(currentDir.getNames());
+                
+
+                return  `cat ${token}: No such file or directory`;
+            })();
+
             functions.insertOutput(output);
-
         }
 
     },
     ls: (input) => {
 
-        for (let i = 0; i< files.length; i++){
 
-            functions.insertOutput(files[i]);
+        currentDir.fileInfo();
+        for (let i = 0; i< currentDir.children.length; i++){
+
+            functions.insertOutput(currentDir.children[i].name);
 
         }
     },
     pwd: (input) => {
-        functions.insertOutput("Users/Nasr");
+
+        const desiredID = currentDir.uniqueID;
+
+        let finished = false;
+        let value = [];
+
+        function recurse(rootNode, path){
+
+            if (!rootNode || finished){
+                return;
+            }
+
+            console.log(path);
+            const pathPrime = path.slice();
+            pathPrime.push(rootNode.name);
+
+            if (rootNode.uniqueID === desiredID){
+                finished = true;
+                value = pathPrime;
+                return;
+            }
+
+            const name = rootNode.name;
+            for (let i = 0; i < rootNode.children;i++){
+                let node = rootNode.children[i];
+                recurse(node, pathPrime)
+            }
+        }
+        recurse(rootDir, []);
+        const finalPrime = value.join("/");
+        functions.insertOutput(finalPrime);
+    },
+    cmds : (input) => {
+        for (let i = 0; i< commands.length; i++){
+
+            functions.insertOutput(commands[i]);
+        }
+    },
+    echo: (input) => {
+        for (let i = 0; i< input.length; i++){
+
+            functions.insertOutput(input[i]);
+        }
+    }, 
+    whoami: (input) => {
+
+        functions.insertOutput('Nasr Syed Maswood');
+
+    },
+    man : (input) => {
+
+        for (let index = 0; index < input.length; input ++){
+            let token = input[length];
+            let output = token in manuals? `${token}\n--->${manuals[token]}`: `No manual entry for ${token}`;
+            functions.insertOutput(output);
+        }
+
+    },
+
+    mkdir: (input) => {
+
+        for (let index = 0; index < input.length; i++){
+
+            let token = foo;
+
+            const newFile = File();
+
+        }
+    },
+    cd: (input) => {
+
+        console.log(input);
+        if (input.length == 0 ){
+            currentDir = rootDir;
+            return
+        }
+
+        const tokenized = input.join("").split("/");
+
+        const result = currentDir;
+
+        if (error){
+            functions.insertOutput(`bash: cd: ${input}: No such file or directory`);
+            return;
+        } 
+    }, 
+    blog : (input) => {
+        window.location.href = 'blog.html';
+    }, 
+    date: (input) => {
+        const time = new Date();
+        const isoString = time.toISOString();
+        functions.insertOutput(isoString);
     }
 }
 
@@ -117,6 +329,8 @@ const functions = {
             }
         }
 
+        removeLastInput();
+
         currentID = ID;
         ID += 1;
 
@@ -150,10 +364,23 @@ const functions = {
 
 function removeLastInput(){
 
+    const doc = document;
+    const line = doc.getElementById(`line-${currentID}`);
+
+    if (line != null){
+        line.removeChild(line.childNodes[1])
+    }
 }
 
-function createClickAble(){
+function filePresentInDir(name, directory){
+    return name in directory.getNames();
 }
+
+function returnFileInDir(name, directory){
+
+}
+
+
 
 function init(){
     const doc = document;
